e818379a411b9e73a59ac3b195dd477dd1f030cc
noah the goodra <peterpan0413@live.com>
2016-10-30 17:13:13 -0500

    Create code.js

---
+++ code.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name         webassign edits
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       jfmherokiller
+// @match        http://www.webassign.net/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    jQuery(".watexline").each(function(index2,item) {
+        //add exponent sign to the problem and unwrap it
+        jQuery( this ).find("sup").each(function() {
+            jQuery( this ).text(function( index,text ) {
+                return "^" + text;
+            });
+            jQuery( this ).replaceWith(this.childNodes);
+        });
+        jQuery(this).find(".watexnumerator,.watexdenominator").each(function() { 
+            jQuery(this).text("("+ jQuery(this).text() +")");
+        });
+        jQuery(this).find(".watexnumerator").each(function() {
+            jQuery(this).replaceWith(jQuery(this).text()+"/");
+        });
+        jQuery(this).find(".watexnumerator,.watexdenominator").each(function() {
+            jQuery(this).replaceWith(jQuery(this).text());
+        });
+        jQuery(this).find(".watexfraction > tbody > tr").each(function()
+                                                   {
+            var t = jQuery(this);
+            var n = t.next();
+            t.html(t.html() + n.html());
+            n.remove();
+        });
+    });
+})();
